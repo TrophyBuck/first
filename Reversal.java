8daa8d8b327184122737bf76fa25a3d01546be6c
TrophyBuck <cdbuck12@gmail.com>
2015-10-17 17:28:09 -0400

    Loop reading input into List

--- Reversal.java
+++ Reversal.java
@@ -1,12 +1,19 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Reversal
 {
 	public static void reverseFile(File input, File output) throws FileNotFoundException
 	{
+		ArrayList<String> lines = new ArrayList<>();
+		Scanner asdf = new Scanner(input);
 		
+		while(asdf.hasNextLine())
+		{
+			lines.add(asdf.nextLine());
+		}
 	}
 }
