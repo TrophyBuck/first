5b2a97d6115ba0a9ce1ace781ffac0eb820265f9
TrophyBuck <cdbuck12@gmail.com>
2015-10-18 13:22:19 -0400

    Reversal code finished

--- Reversal.java
+++ Reversal.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
@@ -15,5 +16,25 @@ public class Reversal
 		{
 			lines.add(asdf.nextLine());
 		}
+		
+		ArrayList<String> temp;
+		String temp2 = "";
+		
+		//Stuff left to do: reverse order of each line in for loop, splitting each line into a list of strings and reversing that list
+		for( int i = 0; i < lines.size(); i++)
+		{
+			temp = new ArrayList<String>(Arrays.asList(lines.get(i).split(" ")));
+			
+			Collections.reverse(temp);
+			
+			for(int j = 0; j < temp.size(); j++)
+			{
+				temp2 = temp2 + temp.get(j);
+			}
+			
+			lines.set(i, temp2);
+		}
+		
+		Collections.reverse(lines);
 	}
 }
