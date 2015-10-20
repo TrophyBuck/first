4187eada6d67e91cf621ccb81d958a02939f099f
TrophyBuck <cdbuck12@gmail.com>
2015-10-19 23:21:24 -0400

    Junit Testing & Output Finished

--- Reversal.java
+++ Reversal.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -7,34 +8,59 @@ import java.util.Scanner;
 
 public class Reversal
 {
-	public static void reverseFile(File input, File output) throws FileNotFoundException
+	
+	public static void reverseFile(File input, File output)
+			throws FileNotFoundException
 	{
-		ArrayList<String> lines = new ArrayList<>();
-		Scanner asdf = new Scanner(input);
-		
-		while(asdf.hasNextLine())
+		try
 		{
-			lines.add(asdf.nextLine());
-		}
-		
-		ArrayList<String> temp;
-		String temp2 = "";
-		
-		//Stuff left to do: reverse order of each line in for loop, splitting each line into a list of strings and reversing that list
-		for( int i = 0; i < lines.size(); i++)
-		{
-			temp = new ArrayList<String>(Arrays.asList(lines.get(i).split(" ")));
-			
-			Collections.reverse(temp);
-			
-			for(int j = 0; j < temp.size(); j++)
+			ArrayList<String> lines = new ArrayList<>();
+			Scanner asdf = new Scanner(input);
+			PrintWriter printer = new PrintWriter(output);
+
+			while (asdf.hasNextLine())
 			{
-				temp2 = temp2 + temp.get(j);
+				lines.add(asdf.nextLine());
 			}
-			
-			lines.set(i, temp2);
+
+			ArrayList<String> temp;
+			String temp2 = "";
+
+			// Stuff left to do: reverse order of each line in for loop,
+			// splitting each line into a list of strings and reversing that
+			// list
+			for (int i = 0; i < lines.size(); i++)
+			{
+				temp = new ArrayList<String>(Arrays.asList(lines.get(i).split(
+						" ")));
+
+				Collections.reverse(temp);
+
+				// set to first word, then add all other words with spaces
+				// between them
+				temp2 = temp.get(0);
+				for (int j = 1; j < temp.size(); j++)
+				{
+					temp2 = temp2 + " " + temp.get(j);
+				}
+
+				lines.set(i, temp2);
+			}
+
+			Collections.reverse(lines);
+
+			// write to file
+			for (int i = 0; i < lines.size(); i++)
+			{
+				printer.write(lines.get(i) + System.lineSeparator());
+			}
+
+			asdf.close();
+			printer.close();
 		}
-		
-		Collections.reverse(lines);
+		catch (Exception e)
+		{
+			throw new FileNotFoundException();
+		}
 	}
 }
